b180f9611d5f49ed9414a950e1bfd2790a279509
qipi <ebroberts9@icloud.com>
2024-09-20 19:31:54 +0100

    JAVA SCRIPT OWOWOWWWWW
    huray

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function abt() {
+    var x = document.getElementById("abt");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+function rainbow() {
+  var x = document.getElementById("rainbow");
+}
